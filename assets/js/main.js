5a9933c003b2e853911b5c2298c014b66fca08b9
VladislavMolodtsov <vladi28061750@gmail.com>
2023-02-26 20:45:38 +0200

    create section footer and element back to scroll top

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,18 +47,20 @@ window.addEventListener('DOMContentLoaded', () => {
         */
 
         const header = document.querySelector('[data-header]');
-        // const backTopBtn = document.querySelector('[data-back-top-btn]');
+        const backTopBtn = document.querySelector('[data-back-top-btn]');
 
         const activeElemOnScroll = function() {
 
                 if (window.scrollY > 100) {
 
                         header.classList.add('active');
-                        // backTopBtn.classList.add('active');
+                        backTopBtn.classList.add('active');
 
+                } else {
+                        header.classList.remove('active');
+                        backTopBtn.classList.remove('active');
                 }
-                header.classList.remove('active');
-                // backTopBtn.classList.remove('active');
+                
         }
 
         addEventOnElem(window, 'scroll', activeElemOnScroll);
@@ -93,7 +95,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
                                 if (filterBtn[i].dataset.filterBtn === item.dataset.filter) {
                                         item.style.display = 'block';
-                                        filterBtn[i].classList.add("active");
+                                        filterBtn[i].classList.add('active');
                                 } else {
                                         item.style.display = 'none';
                                         filterBtn[i].classList.remove('active');
@@ -110,4 +112,5 @@ window.addEventListener('DOMContentLoaded', () => {
                         showTab(i);
                 });
         });
+
 });
